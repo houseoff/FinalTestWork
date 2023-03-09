3b5608b8966a5e0a5e00534d0200d4a5e82e0b3c
E. Khaustov <hrust656@gmail.com>
2023-03-09 16:25:24 +1000

    v2.0. function DesiredArray was deleted. Instead of a the function, used System.Linq. The amount of code has decreased

--- Program.cs
+++ Program.cs
@@ -1,17 +1,8 @@
-﻿string[] DesiredArray(string[] array)
-{
-    string[] da = new string[array.Length];
-    for (int i = 0; i < array.Length; i++)
-        if (array[i].Length <= 3)
-            da[i] = array[i];
-    return da;
-}
-
-Console.Clear();
+﻿Console.Clear();
 Console.Write("Введите элементы массива через запятую: ");
-string[] array = Console.ReadLine()!.Split(',');
-string[] da = DesiredArray(array);
-if (da.Length == 0)
+string[] array = Console.ReadLine()!.Split(',').Select(x => x.Trim()).Where(x => x.Length <= 3).ToArray();
+Console.WriteLine($"Длина массива: {array.Length}");
+if (array.Length < 1 || string.IsNullOrWhiteSpace(array[0]))
     Console.WriteLine($"Cтрок, длина которых меньше, либо равна трём символам, не найдено");
 else
-    Console.WriteLine($"Массив строк, длина которых меньше, либо равна трём символам: [{string.Join(',', da)}]");
+    Console.WriteLine($"Массив строк, длина которых меньше, либо равна трём символам: [{string.Join(',', array)}]");
