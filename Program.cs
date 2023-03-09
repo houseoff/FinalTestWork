5acfcc192f018374142a3ac813fb1d5e62d4d48a
E. Khaustov <hrust656@gmail.com>
2023-03-09 16:46:11 +1000

    v2.1. Added a null or white space check condition to the 'where' condition block. A little bit changed formatting of the code

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
 ﻿Console.Clear();
 Console.Write("Введите элементы массива через запятую: ");
-string[] array = Console.ReadLine()!.Split(',').Select(x => x.Trim()).Where(x => x.Length <= 3).ToArray();
-Console.WriteLine($"Длина массива: {array.Length}");
-if (array.Length < 1 || string.IsNullOrWhiteSpace(array[0]))
+string[] array = Console.ReadLine()!
+    .Split(',')
+    .Select(x => x.Trim())
+    .Where(x => (x.Length <= 3 & !string.IsNullOrWhiteSpace(x)))
+    .ToArray();
+
+if (array.Length < 1)
     Console.WriteLine($"Cтрок, длина которых меньше, либо равна трём символам, не найдено");
 else
     Console.WriteLine($"Массив строк, длина которых меньше, либо равна трём символам: [{string.Join(',', array)}]");
